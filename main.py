492164038787bd5349a7cca1a9747228b909a47e
kraujinis <kraujinis@gmail.com>
2023-05-15 22:23:36 +0300

    save main.py nothing changes

--- main.py
+++ main.py
@@ -3,28 +3,31 @@ from pymongo.database import Database
 from pymongo.collection import Collection
 from typing import Dict, List
 
+
 class MongoDb:
     # Create(Insert)
     def insert_create_document(self, collection: Collection, document: Dict) -> str:
         result = collection.insert_one(document)
         return str(result.inserted_id)
-    
+
     # Read
     def find_documents(self, collection: Collection, query: Dict) -> List[Dict]:
         documents = collection.find(query)
         return list(documents)
-    
+
     # Update
     def update_document(self, collection: Collection, query: Dict, update: Dict) -> int:
         result = collection.update_many(query, {"$set": update})
         return result.modified_count
-    
+
     # Delete
     def delete_documents(self, collection: Collection, query: Dict) -> int:
         result = collection.delete_many(query)
         return result.deleted_count
-    
-    def get_database_collection(self, database: Database, collection_name: str) -> Collection:
+
+    def get_database_collection(
+        self, database: Database, collection_name: str
+    ) -> Collection:
         collection = database[collection_name]
         return collection
 
