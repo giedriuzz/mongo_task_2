3823812cd17d38d00ad9a0b1c629cd6546137b45
Vladas <vladas.budvytis@gmail.com>
2023-05-16 20:13:23 +0300

    Problems with read and update in app.py; read show all db elements, update not updates; little changes in main.py

--- app.py
+++ app.py
@@ -7,49 +7,74 @@ from pymongo.collection import Collection
 # mongo config
 mongodb_host = 'localhost'
 mongodb_port = 27017
-database_name = 'tasks'
+database_name = 'Tasks'
 collection_name = 'user_tasks'
 
-mongo = MongoDb()
+mongo = MongoDb(mongodb_host, mongodb_port, database_name, collection_name)
 # connection
 db = connect.connect_to_mongodb(mongodb_host, mongodb_port, database_name)
 collection = db[collection_name]
 
-document = {
-    "task_name": "Wash car",
-    "task_description": 'Wash car only with water',
-    "task_status": "started",
-    "user_name": "Tadas Tadas",
-    "user_email": "tadas@gmail.com",
-}
-mongo.insert_create_document(collection=collection, document=document)
+# document = {
+#     "task_name": "Wash car",
+#     "task_description": 'Wash car only with water',
+#     "task_status": "started",
+#     "user_name": "Tadas Tadas",
+#     "user_email": "tadas@gmail.com",
+# }
+# mongo.insert_create_document(collection=collection, document=document)
+
+def create_new_task() -> None:
+    t_name = input("Please insert task name: ")
+    t_deskription = input("Enter task description: ")
+    t_status = input("Task status (ex.: 'not started'): ")
+    u_name = input("User name: ")
+    u_email = input("User email: ")
+    in_task = {
+        "task_name": t_name, 
+        "task_description": t_deskription, 
+        "task_status": t_status, 
+        "user_name": u_name, 
+        "user_email": u_email, 
+    }
+    task = mongo.insert_create_document(collection=collection, document=in_task)
+
+    print(f"Task added with ID: {task}")  
+
+
+def view_all_tasks():
+    slc_email = str(input("Enter email, which tasks show: "))
+    collections = mongo.find_documents(query=slc_email)
+    print("Your tasks:")
+    for collect in collections:
+        # print(f"ID: {collect['id']}")
+        print(f"Task name: {collect['task_name']}")
+        print(f"Task description: {collect['task_description']}")
+        print(f"Task status: {collect['task_status']}")
+        print(f"User name: {collect['user_name']}")
+        print(f"User email: {collect['user_email']}")
+        print("**************************************************")
+
+
+def update_task():
+    task_id = input("Enter task ID: ")
+    status = input("Enter task status: ")
+    update_status = mongo.update_document(task_id, status)
+    print(f"{update_status} task(s) updated.")
+
+
 
 while True:
     selection = int(input("Select what to do: \n1 - Read \n2 - Create \n3 - Update \n4 - Delete \n5 - Exit\n"))
 
     if selection == 1:
-        collections = mongo.get_database_collection(db, collection_name)
-        print(collections)  
+        view_all_tasks()
 
     if selection == 2:
-        t_name = input("Please insert task name: ")
-        t_deskription = input("Enter task description: ")
-        t_status = input("Task status (ex.: 'not started'): ")
-        u_name = input("User name: ")
-        u_email = input("User email: ")
-        in_task = {
-            "task_name": t_name, 
-            "task_description": t_deskription, 
-            "task_status": t_status, 
-            "user_name": u_name, 
-            "user_email": u_email, 
-        }
-        task = mongo.insert_create_document(collection=collection, document=in_task)
-
-        print(f"Task added with ID: {task}")
+        create_new_task()
 
     if selection == 3:
-        print("Would be Update")
+        update_task()
 
     if selection == 4:
         print("Would be Delete")
--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 from pymongo import MongoClient
 from pymongo.database import Database
 from pymongo.collection import Collection
-from typing import Dict, List
+from typing import Dict, List, Any
 
 class MongoDb:
+    def __init__(self, host: str, port: int, db_name: str, collection_name: str):
+        self.client = MongoClient(host, port)
+        self.db = self.client[db_name]
+        self.collection = self.db[collection_name]
+        
     # Create(Insert)
     def insert_create_document(self, collection: Collection, document: Dict) -> str:
-        result = collection.insert_one(document)
+        result = self.collection.insert_one(document)
         return str(result.inserted_id)
     
     # Read
-    def find_documents(self, collection: Collection, query: Dict) -> List[Dict]:
-        documents = collection.find(query)
-        return list(documents)
+    def find_documents(self, query: str) -> List[Dict[str, Any]]:
+        # documents = self.collection.find_one(query)
+        return list(self.collection.find())
     
     # Update
-    def update_document(self, collection: Collection, query: Dict, update: Dict) -> int:
-        result = collection.update_many(query, {"$set": update})
+    def update_document(self, task_id: str, status: str) -> int:
+        result = self.collection.update_one({'_id': task_id}, {'$set': {'task_status': status}})
         return result.modified_count
     
     # Delete
@@ -26,7 +31,7 @@ class MongoDb:
     
     def get_database_collection(self, database: Database, collection_name: str) -> Collection:
         collection = database[collection_name]
-        return collection
+        return list(collection.find())
 
     def list_databases(self, client: MongoClient) -> List[str]:
         return client.list_database_names()
