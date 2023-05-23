a93bbf3704b20bf1d48f0bbfa047ae70a3c05064
kraujinis <kraujinis@gmail.com>
2023-05-23 22:16:18 +0300

    some not minor changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import connect
+from connect import connect
 from main import MongoDb
 from pymongo.collection import Collection
 
@@ -6,37 +6,34 @@ from pymongo.collection import Collection
 # mongo config
 mongodb_host = "localhost"
 mongodb_port = 27017
-database_name = "tasks"
-collection_name = "user_tasks"
+database_name = "raspberryDb"
+collection_name = "raspberryCol"
 
 mongo = MongoDb()
 # connection
-db = connect.connect_to_mongodb(mongodb_host, mongodb_port, database_name)
+db = connect.Connect().connect_to_mongodb(mongodb_host, mongodb_port, database_name)
 collection = db[collection_name]
 
 # --- insert
+document = {"name": "Pilsutskis Marius", "age": 15, "email": "pilsustskis@gmail.com"}
+mongo.insert_create_document(collection=collection, document=document)
+
 # document = {
-#     "name": "Pilsutskis Marius",
-#     "age": 15,
-#     "email": "pilsustskis@gmail.com"
+#     "task_name": "Wash car",
+#     "task_description": "Wash car only with water",
+#     "task_status": "started",
+#     "user_name": "Tadas Tadas",
+#     "user_email": "tadas@gmail.com",
 # }
 # mongo.insert_create_document(collection=collection, document=document)
 
-document = {
-    "task_name": "Wash car",
-    "task_description": "Wash car only with water",
-    "task_status": "started",
-    "user_name": "Tadas Tadas",
-    "user_email": "tadas@gmail.com",
-}
-mongo.insert_create_document(collection=collection, document=document)
-
-# Read
-query = {"age": 15}
-results = mongo.find_documents(collection=collection, query=query)
-print(results)
+# # Read
+# query = {"age": 15}
+# results = mongo.find_documents(collection=collection, query=query)
+# print(results)
 
 
 # get collections
 collections = mongo.get_database_collection(db, collection_name)
-print(collections)
+for c in collections:
+    print(c)
